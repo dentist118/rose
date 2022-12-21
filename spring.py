42bd18d15654a70afdacde5f888585b7da48a463
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 04:01:26 +0200

    Update spring.py

--- spring.py
+++ spring.py
@@ -1,28 +1,30 @@
+
 import streamlit as st
 
 from PIL import Image
 
 
-#def test_st_image_PIL_array(self):
-"""DR.MAHMOUD ABD ELAZIZ."""
+
+"""Test st.image with a PIL array."""
 imgs = [
-            Image.new("RGB", (32, 32), color="red"),
-            Image.new("RGB", (32, 32), color="GRAY"),
-            Image.new("RGB", (64, 64), color="BLACK"),
+            Image.new("RGB", (64, 64), color="red"),
+            Image.new("RGB", (64, 64), color="blue"),
+            Image.new("RGB", (64, 64), color="green"),
         ]
         # Manually calculated by letting the test fail and copying and
         # pasting the result.
-imgs_b32 = [
-            "medo1.png",
-            "medo1.png",
-            "medo1.png",
+imgs_b64 = [
+            "A1oDWgNaA9oFUoUBf3Xr7AgAAAAASUVORK5CYII=",
+            "WgNaA1oDWgPaBVCHAX/y3CvgAAAAAElFTkSuQmCC",
+            "NaA1oDWgNaBdYVwBALVjUB8AAAAASUVORK5CYII=",
         ]
 st.image(
             imgs,
             caption=["some caption"] * 3,
-            width=30,
+            width=200,
             use_column_width=True,
             clamp=True,
             
         )
 
+       
