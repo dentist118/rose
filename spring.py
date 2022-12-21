cfea47164c89648fe7c46dee201cf50ef2f7a5d2
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 03:27:38 +0200

    Add files via upload

---
+++ spring.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+from PIL import Image
+
+
+#def test_st_image_PIL_array(self):
+"""Test st.image with a PIL array."""
+imgs = [
+            Image.new("RGB", (32, 32), color="orange"),
+            Image.new("RGB", (32, 32), color="pink"),
+            Image.new("RGB", (64, 64), color="yellow"),
+        ]
+        # Manually calculated by letting the test fail and copying and
+        # pasting the result.
+
+st.image(
+            Image.open('C:\medo1.png'),
+            
+            caption=["some caption"] * 1,
+            width=30,
+            use_column_width=True,
+            clamp=True,
+           
+            
+        )
+
