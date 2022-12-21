235b2fe16103d39e162412077fa64bee3b5e559e
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 04:49:04 +0200

    Update spring.py

--- spring.py
+++ spring.py
@@ -8,8 +8,7 @@ from PIL import Image
 """Test st.image with a PIL array."""
 imgs = [
             Image.new("RGB", (64, 64), color="red"),
-            Image.new("RGB", (64, 64), color="blue"),
-            Image.new("RGB", (64, 64), color="green"),
+            
         ]
         # Manually calculated by letting the test fail and copying and
         # pasting the result.
@@ -17,7 +16,7 @@ url = "https://streamlit.io/cat.png"
 caption = "ahoy!"
 
         
-st.image([url] * 2, caption=[caption] * 2)
+st.image([url] * 1, caption=[caption] * 1)
 st.image(
             imgs,
             caption=["some caption"] * 3,
