2448d105d3c126227283338eaae93bd87be7c203
Dr.Mahmoud <82735653+dentist118@users.noreply.github.com>
2022-12-21 21:59:41 +0200

    Update spring.py

--- spring.py
+++ spring.py
@@ -3,13 +3,29 @@ import streamlit as st
 
 from PIL import Image
 
+def add_bg_from_url():
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+add_bg_from_url() 
 
 
-"""Test st.image with a PIL array."""
-imgs = [
-            Image.new("RGB", (64, 64), color="pink"),
+
+"""NEW PROJECT."""
+#imgs = [
+          #  Image.new("RGB", (64, 64), color="pink"),
             
-        ]
+     #   ]
         # Manually calculated by letting the test fail and copying and
         # pasting the result.
 url = "https://bgremoval.streamlit.app/~/+/media/c4e5367c557d51b94574671c91778cff53fb5aff96f31ed81ece3e63.jpg"
